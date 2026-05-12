b93a3c5147cc0421578e0baffc32b89a4ab2eae4
AbdullahSudo <abdullahliet69@gmail.com>
2026-05-12 06:17:36 +0530

    final vercel setup

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # 🔥 CHANGE THIS to your real project folder name
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Currency_Master.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Currency.settings")
 
 application = get_wsgi_application()
